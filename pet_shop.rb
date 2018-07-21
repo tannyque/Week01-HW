e0d09a768552e2c3506148a4bdae162bff3281d6
tannyque <tannyque1992@gmail.com>
2018-07-21 19:29:11 +0100

    Passes increase pets sold test

--- pet_shop.rb
+++ pet_shop.rb
@@ -6,10 +6,14 @@ def total_cash(money)
   return money[:admin][:total_cash]
 end
 
-def add_or_remove_cash(pet_shop, num)
-  pet_shop[:admin][:total_cash] += num
+def add_or_remove_cash(pet_shop, cash)
+  pet_shop[:admin][:total_cash] += cash
 end
 
 def pets_sold(pet_shop)
   return pet_shop[:admin][:pets_sold]
+end
+
+def increase_pets_sold(pet_shop, sold)
+  pet_shop[:admin][:pets_sold] += sold
 end
